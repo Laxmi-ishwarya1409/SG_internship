d5bd974bfd98c541eada44b82d2a8e10d5367dbc
LAXMI ISHWARYA GOPATHI <laxmiishwarya0914@gmail.com>
2025-10-08 11:13:39 +0530

    Update 01_calculator.py

--- PYTHON/01_calculator.py
+++ PYTHON/01_calculator.py
@@ -1,11 +1,5 @@
 # Create a simple calculator that performs addition, subtraction, multiplication, and division.
 
-
-
-class StrError(Exception):
-   pass
-
-
 def calculator():
     running = True
     while running:
